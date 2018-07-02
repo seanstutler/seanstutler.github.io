2194a2e67e250cb686a7490b3d2b950aef439228
seanstutler <seanunbelievably@gmail.com>
2018-07-03 00:31:01 +0800

    but to be fixed

--- gravity-ball/main.js
+++ gravity-ball/main.js
@@ -1,14 +1,43 @@
 "use strict";
 (function() {
+    const G = 9.81;
+    const SPEED = 400;
+    let timer;
+    let xspeed;
+    let yspeed;
+    const REFRESH = 60;
+
     window.onload = function() {
         reset();
         $("reset").onclick = reset;
+        $("go").onclick = start;
+    }
+
+    function start() {
+        let angle = $("angle").value;
+        xspeed = SPEED * Math.sin(angle);
+        yspeed = SPEED * Math.cos(angle);
+        timer = setInterval(update, 1 / REFRESH * 1000);
+    }
+
+    function update() {
+        let ball = $("ball");
+        ball.style.left = parseFloat(ball.style.left) + xspeed * (1 / REFRESH) + "px";
+        ball.style.top = parseFloat(ball.style.top) - (yspeed - G * 1 / REFRESH) * (1 / REFRESH) + "px";
     }
 
     function reset() {
+        $("container").innerHTML = "";
+        clearInterval(timer);
         let ball = gen("div");
         ball.id = "ball";
         ball.classList.add("ball");
+        let radius = 20;
+        ball.style.left = "0px";
+        ball.style.height = radius + "px";
+        ball.style.width = radius + "px";
+        ball.style.top = parseInt(window.getComputedStyle($("container")).height) - radius - 2 + "px";
+        $("container").appendChild(ball);
     }
 
 
